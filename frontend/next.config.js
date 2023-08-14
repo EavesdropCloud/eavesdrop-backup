6cb7eb61f29d23510765e6782545ccd1b2ab7ad1
voidinterface <cameron@voidinterface.net>
2023-08-14 17:51:40 +1200

    Deploy frontend development server to docker

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config, context) => {
+        if(process.env.NEXT_WEBPACK_USEPOLLING) {
+            config.watchOptions = {
+            poll: 500,
+            aggregateTimeout: 300
+            }
+        }
+        return config
+    },
+}
 
 module.exports = nextConfig
