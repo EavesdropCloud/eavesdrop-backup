81c781b9471467914ceb8a695bd13be96aad2f90
marikitstroud <marikitstroud@gmail.com>
2023-09-04 22:20:51 +1200

    Page layout and formatting

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -5,8 +5,10 @@ export default async function Home() {
   return (
     <main>
       <h1 className="text-xl text-center font-semibold mt-10">About Eavesdrop</h1>
-      <div className="m-50 flex justify-center border-2 border-white">
-        <h1>--Coming Soon--</h1>
+      <div className="flex justify-center">
+        <p className="p-300">Eavesdrop is a cloud hosted web application designed with DJs in mind. Store your local files on the cloud instead, download them from anywhere
+          and access your setlists whenever and wherever you want. 
+        </p>
       </div>
     </main>
   )
--- frontend/src/app/account/page.tsx
+++ frontend/src/app/account/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   return (
     <main>
       <h1 className="text-xl text-center font-semibold mt-10">Your account</h1>
-      <div className="m-50 flex justify-center border-2 border-white">
+      <div className="m-50 flex justify-center">
         <h1>--Coming Soon--</h1>
       </div>
     </main>
