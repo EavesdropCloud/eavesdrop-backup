0306e9575ca2997fd5e7c7b7720d88a7e9f42340
marikitstroud <marikitstroud@gmail.com>
2023-09-04 16:17:46 +1200

    Add custom colour scheme to tailwind

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -6,18 +6,22 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  // theme: {
-  //   extend: {
-  //     backgroundImage: {
-  //       'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-  //       'gradient-conic':
-  //         'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-  //     },
-  //   },
-  //   colors: {
-  //     'midnight': '#170a1c',
-  //   },
-  // },
+   theme: {
+     extend: {
+      //  backgroundImage: {
+      //    'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //    'gradient-conic':
+      //      'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      //  },
+      colors: {
+        'midnight': '#170a1c',
+        'darkpurple': '#391833',
+        'lilac': '#BE95C4',
+        'lightpink': '#E0B1CB',
+        'powder': '#F5F0F1',
+      },
+     },
+   },
   plugins: [],
 }
 export default config
