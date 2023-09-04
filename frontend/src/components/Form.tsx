b0264395966c2f315d808f938efb86902488db54
marikitstroud <marikitstroud@gmail.com>
2023-09-04 22:21:39 +1200

    Format and style form

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -21,13 +21,23 @@ const Form = ({ song }) => {
     };
 
     return (
-        <main>
-            <form onSubmit={onSubmit}>
-                <input type="text" name="title" defaultValue={song.metadata.title} required/>
-                <input type="text" name="artist" defaultValue={song.metadata.artist} required/>
-                <button type="submit">Submit</button>
-            </form>
-        </main>
+        <form className="" onSubmit={onSubmit}>
+            <h1 className="text-2xl text-left font-semibold pt-10 px-20">Edit song details</h1>
+            <div className="flex flex-col">
+                <div className="my-5 mx-20">
+                    <label className="block font-bold" for="song-title">Title</label>
+                    <input className="bg-midnight-100" type="text" name="title" id="song-title"defaultValue={song.metadata.title} required/>
+                </div>
+                <div className="mb-5 mx-20">
+                    <label className="block font-bold" for="artist-name">Artist</label>
+                    <input className="bg-midnight-100" type="text" name="artist" id="artist-name" defaultValue={song.metadata.artist} required/>
+                </div>
+                <div className="mb-5 mx-20">
+                    <button className="px-3 py-2 rounded-md opacity-80 transition duration-300 ease-in-out bg-transparent hover:opacity-100 hover:bg-midnight-150"
+                            type="submit">Submit</button>
+                </div>
+            </div>
+        </form>
     );
 }
 
