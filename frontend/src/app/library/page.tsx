1d4fce75aa3f461c21fdb2127782523a18d4f403
marikitstroud <marikitstroud@gmail.com>
2023-10-01 17:31:40 +1300

    Move library to its own page;

--- frontend/src/app/library/page.tsx
+++ frontend/src/app/library/page.tsx
@@ -5,8 +5,16 @@ export default async function Home() {
   const songs = await getSongs()
   return (
     <main className="px-30">
-      <h1 className="text-2xl text-left font-semibold pt-10 px-20">Welcome to Eavesdrop!</h1>
-      
+      <h1 className="text-2xl text-left font-semibold pt-10 px-20">Song Library</h1>
+      <div className="grid grid-cols-[50%,30%,10%] gap-4 pl-20 py-10 ml-5">
+        <div className="opacity-60">Title</div>
+        <div className="opacity-60">Artist</div>
+        <div></div>
+
+        {songs.map((song: SongProps, key: any) => (
+          <SongRow song={song} key={key}/>
+        ))}
+      </div>
     </main>
   )
 }
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Sidebar = () => (
     <div className="flex flex-col content-center bg-gradient-to-r from-midnight-50 to-midnight-100 text-powder w-40">
         <div className="my-3 mx-20 opacity-50 transition duration-300 ease-in-out bg-transparent hover:opacity-100">
-            <Link href="/">Library</Link>
+            <Link href="/library">Library</Link>
         </div>
         <div className="my-3 mx-20 opacity-50 transition duration-300 ease-in-out bg-transparent hover:opacity-100">
             <Link href="/upload">Upload</Link>
