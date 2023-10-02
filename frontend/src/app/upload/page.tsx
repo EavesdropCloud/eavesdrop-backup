396fa20579ea99b80d8af1163cbb014b18619efd
marikitstroud <marikitstroud@gmail.com>
2023-10-02 21:03:31 +1300

    Upload multiple files at a time

--- frontend/src/app/upload/page.tsx
+++ frontend/src/app/upload/page.tsx
@@ -7,23 +7,25 @@ import { useState } from 'react'
 const Upload = () => {
   const [message, setMessage] = useState<string>('');
   const handleUpload = async (files: File[]) => {
-    const file = files[0];
-    const formData = new FormData();
-    formData.append('file', file);
+    files.forEach( async file =>  {
+      const formData = new FormData();
+      formData.append('file', file);
+  
+      setMessage(`Uploading files...`);
+      try {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        const res = await fetch('/api/files/upload', {
+          method: 'POST',
+          body: formData
+        }).then(res => {
+          setMessage("Files uploaded! Check your song library, or upload more.");
+        });
+      } catch (error) {
+        console.error('Error uploading file.', error);
+        setMessage('Error uploading files.');
+      }
+    });
 
-    setMessage("Uploading file...");
-    try {
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      const res = await fetch('/api/files/upload', {
-        method: 'POST',
-        body: formData
-      }).then(res => {
-        setMessage("File uploaded! Check your song library, or upload more.");
-      });
-    } catch (error) {
-      console.error('Error uploading file.', error);
-      setMessage('Error uploading file.');
-    }
   }
 
   return (
