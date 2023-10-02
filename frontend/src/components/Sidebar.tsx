73aeb48d2a875a877950538192762e6bc6e88c8b
marikitstroud <marikitstroud@gmail.com>
2023-10-03 01:24:39 +1300

    Minor formatting and changes

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -3,11 +3,13 @@ import React from "react";
 
 const Sidebar = () => (
     <div className="flex flex-col content-center bg-raisin-100 border-r border-r-raisin-200 text-powder w-40">
-        <div className="my-3 mx-20 opacity-50 transition duration-300 ease-in-out bg-transparent hover:opacity-100">
-            <Link href="/library">Library</Link>
+        <div className="mx-5 pl-2 my-2 w-20">
+            <Link className="pl-3 opacity-50 transition duration-300 ease-in-out bg-transparent hover:opacity-100 focus:border-l-2 
+                    focus:border-l-pinkman focus:font-bold focus:opacity-100" href="/library">Library</Link>
         </div>
-        <div className="my-3 mx-20 opacity-50 transition duration-300 ease-in-out bg-transparent hover:opacity-100">
-            <Link href="/upload">Upload</Link>
+        <div className="ml-5 pl-2 mb-2">
+            <Link className="pl-3 opacity-50 transition duration-300 ease-in-out bg-transparent hover:opacity-100 focus:border-l-2 
+                    focus:border-l-pinkman focus:font-bold focus:opacity-100" href="/upload">Upload</Link>
         </div>
     </div>
 );
