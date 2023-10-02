58955c0a43d8226cadce9663c9a0cc4dd145f775
marikitstroud <marikitstroud@gmail.com>
2023-10-02 22:25:33 +1300

    Formatting edit form

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -17,27 +17,32 @@ const Form = ({ song }) => {
                 title: formData.get("title"),
                 artist: formData.get("artist")
             }})
-        });
+        })
+        if (typeof window !== 'undefined') {
+            window.location.href = '/library';
+        };
     };
 
     return (
-        <form className="" onSubmit={onSubmit}>
-            <h1 className="text-2xl text-left font-semibold pt-10 px-20">Edit song details</h1>
-            <div className="flex flex-col">
-                <div className="my-5 mx-20">
-                    <label className="block font-bold" for="song-title">Title</label>
-                    <input className="bg-midnight-100" type="text" name="title" id="song-title"defaultValue={song.metadata.title} required/>
+        <div className="w-1/2 h-1/2 m-20 p-5 bg-raisin-200 rounded-3xl shadow-xl">
+            <h1 className="text-2xl p-3 text-left font-semibold">Edit song details</h1>
+            <form className="" onSubmit={onSubmit}>
+                <div className="flex flex-col">
+                    <div className="my-5 pl-10">
+                        <label className="px-2 py-1 block text-sm opacity-50 font-semibold" for="song-title">Title</label>
+                        <input className="w-1/2 p-2 bg-raisin-300 rounded" type="text" name="title" id="song-title"defaultValue={song.metadata.title} required/>
+                    </div>
+                    <div className="mb-5 pl-10">
+                        <label className="px-2 py-1 block text-sm opacity-50 font-semibold" for="artist-name">Artist</label>
+                        <input className="w-1/2 p-2 bg-raisin-300 rounded" type="text" name="artist" id="artist-name" defaultValue={song.metadata.artist} required/>
+                    </div>
+                    <div className="pl-10">
+                        <button className="px-5 py-2 rounded-full opacity-80 transition duration-300 ease-in-out bg-transparent hover:opacity-100 hover:bg-pinkman"
+                                type="submit">Submit</button>
+                    </div>
                 </div>
-                <div className="mb-5 mx-20">
-                    <label className="block font-bold" for="artist-name">Artist</label>
-                    <input className="bg-midnight-100" type="text" name="artist" id="artist-name" defaultValue={song.metadata.artist} required/>
-                </div>
-                <div className="mb-5 mx-20">
-                    <button className="px-3 py-2 rounded-md opacity-80 transition duration-300 ease-in-out bg-transparent hover:opacity-100 hover:bg-midnight-150"
-                            type="submit">Submit</button>
-                </div>
-            </div>
-        </form>
+            </form>
+        </div>
     );
 }
 
