989b8e5c138e68f6f2da3e06e6651961c2968259
voidinterface <cameron@voidinterface.net>
2023-10-02 18:23:12 +1300

    Add workload identity pool for github runner

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,3 +2,8 @@ provider "google-beta" {
   project     = "deft-crawler-400712"
   region      = "us-west1"
 }
+
+provider "google" {
+  project     = "deft-crawler-400712"
+  region      = "us-west1"
+}
--- terraform/repository.tf
+++ terraform/repository.tf
@@ -1,7 +1,48 @@
+data "google_project" "project" {}
+
 resource "google_artifact_registry_repository" "docker-repo" {
-project             = "deft-crawler-400712"
+    project         = "deft-crawler-400712"
     location        = "us-west1"
     repository_id   = "eavesdrop-docker-repository"
     description     = "Eavesdrop Docker Repository."
     format          = "DOCKER"
+}
+
+resource "google_service_account_iam_binding" "docker-repo-iam" {
+  service_account_id = google_service_account.github.name
+  role               = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/EavesdropCloud/eavesdrop"
+  ]
+}
+
+resource "google_service_account" "github" {
+  account_id   = "github"
+  project      = data.google_project.project.project_id
+  display_name = "GitHub"
+}
+
+resource "google_iam_workload_identity_pool" "github" {
+  workload_identity_pool_id = "github"
+}
+
+resource "google_iam_workload_identity_pool_provider" "github" {
+  project                            = data.google_project.project.project_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github"
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.actor"      = "assertion.actor"
+    "attribute.repository" = "assertion.repository"
+  }
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
+resource "google_project_iam_binding" "read-write-registry-iam" {
+  project = data.google_project.project.project_id
+  role    = "roles/artifactregistry.writer"
+  members = ["serviceAccount:${google_service_account.github.email}"]
 }
