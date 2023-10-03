e17379d715fde77de61afbce94d868b49da31c60
voidinterface <cameron@voidinterface.net>
2023-10-03 16:07:54 +1300

    Fix routing issues

--- apps/frontend/src/config/index.ts
+++ apps/frontend/src/config/index.ts
@@ -1,2 +1,2 @@
-export const api_proxy = process.env.WEBSERVER_URL || 'http://localhost:3000/';
-export const api_server = process.env.BACKEND_API_BASE || 'http://localhost:5000/';
+export const api_proxy = process.env.WEBSERVER_URL || 'http://localhost:3000';
+export const api_server = process.env.BACKEND_API_BASE || 'http://localhost:5000';
